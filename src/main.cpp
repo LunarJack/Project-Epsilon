2af698e33b46feba408e64a86b67637f8e2e1aa4
LunarJack <lcolefarrell@icloud.com>
2024-12-20 15:04:08 -0800

    add std

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 int main(int argc, char *argv[]) {
     if (argv = "help") {
-        cout << "Compile:\nepsilon compile <input file path>";
+        std::cout << "Compile:\nepsilon compile <input file path>";
     } else {
-        cout << "Welcome to Epsilon!\nEpsilon is a coding language based on C ++.\nTo see a list of commands type epsilon help.";
+        std::cout << "Welcome to Epsilon!\nEpsilon is a coding language based on C ++.\nTo see a list of commands type epsilon help.";
         return 0;
     }
 }
