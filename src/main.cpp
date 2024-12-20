18d9302400b103b1c1e238de74993707147c8177
LunarJack <lcolefarrell@icloud.com>
2024-12-20 15:06:04 -0800

    fix main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "headers/lexer.hpp"
 #include <iostream>
-int main(int argc, char *argv[]) {
-    if (argv = "help") {
+int main(std::string isHelp) {
+    if (isHelp = "help") {
         std::cout << "Compile:\nepsilon compile <input file path>";
     } else {
         std::cout << "Welcome to Epsilon!\nEpsilon is a coding language based on C ++.\nTo see a list of commands type epsilon help.";
         return 0;
     }
 }
+int 
