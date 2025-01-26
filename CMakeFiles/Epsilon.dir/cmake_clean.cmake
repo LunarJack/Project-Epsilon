e35dab770453bf4de653b7f46565c085a29be129
LunarJack <lcolefarrell@icloud.com>
2025-01-26 13:55:16 -0800

    changed lots of stuff

---
+++ CMakeFiles/Epsilon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Epsilon.dir/src/main.cpp.o"
+  "CMakeFiles/Epsilon.dir/src/main.cpp.o.d"
+  "Epsilon"
+  "Epsilon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Epsilon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
