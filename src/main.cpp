ebdde90cc68d7f662921494ff2ebfb547faf1dff
LunarJack <lcolefarrell@icloud.com>
2025-01-26 16:14:51 -0800

    switch to Qt

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-        if (strcmp(argv[1], "-help") == 0) {
-                cout << "HELP!?" << endl;
-                return 0;
+        if (argc == 2) {
+                if (strcmp(argv[1], "-h") == 0) {
+                        cout << "HELP!?" << endl;
+                        return 0;
+                }
         } else {
-                cout << "Welcome to Epsilon!\nEpsilon is a coding language based on C ++.\nTo see a list of commands type:\nepsilon -help." << endl;
+                cout << "Welcome to Epsilon!\nEpsilon is a coding language based on C ++.\nTo see a list of commands type:\nepsilon -h." << endl;
                 return 0;
         }
 }
