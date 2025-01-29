651e2e374bacfee81b1390619034d35650e5145b
LunarJack <lcolefarrell@icloud.com>
2025-01-29 15:53:45 -0800

    changed things

--- src/include/compile.h
+++ src/include/compile.h
@@ -1,4 +1,4 @@
 #ifndef COMPILE_H
 #define COMPILE_H
-int compile(char *file1NameAndPath[], char *file2NameAndPath[]);
+int compile(char *file1NameAndPath, char *file2NameAndPath);
 #endif // COMPILE_H
